71fcc152a1051c39d93e3da9b3e5288e78620264
halucinka <halucinka@gmail.com>
2018-02-05 15:00:54 -0500

    Change implementation to sort in place.

--- quick_sort/quick_sort.go
+++ quick_sort/quick_sort.go
@@ -3,32 +3,44 @@ package quick_sort
 import "math/rand"
 
 func QuickSort(array []int) []int {
+	return quickSort(array, 0, len(array)-1)
+}
 
-	if len(array) <= 1 {
-		arrayCopy := make([]int, len(array))
-		copy(arrayCopy, array)
-		return arrayCopy
+func quickSort(array []int, left, right int) []int {
+
+	if left >= right {
+		return array
 	}
 
-	pivot := array[rand.Intn(len(array))]
+	array, middle := randomizedPartitioningOfSubarray(array, left, right)
+	array = quickSort(array, left, middle-1)
+	array = quickSort(array, middle+1, right)
 
-	var less, middle, more []int
+	return array
+}
 
-	for _, value := range array {
-		switch {
-		case value < pivot:
-			less = append(less, value)
-		case value == pivot:
-			middle = append(middle, value)
-		case value > pivot:
-			more = append(more, value)
+func randomizedPartitioningOfSubarray(array []int, left, right int) ([]int, int) {
+	pivot := left + rand.Intn(right-left)
+	array = swap(array, right, pivot)
+	return paritioningOfSubarray(array, right, left)
+}
+
+func paritioningOfSubarray(array []int, right int, left int) ([]int, int) {
+	pivotValue := array[right]
+	i := left - 1
+	for j := left; j < right; j++ {
+		if array[j] <= pivotValue {
+			i++
+			array = swap(array, i, j)
 		}
 	}
+	array = swap(array, i+1, right)
+	return array, i + 1
+}
 
-	less = QuickSort(less)
-	more = QuickSort(more)
-
-	less = append(less, middle...)
-	less = append(less, more...)
-	return less
+func swap(array []int, a, b int) []int {
+	tmp := array[a]
+	array[a] = array[b]
+	array[b] = tmp
+	return array
 }
