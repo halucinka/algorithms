2bf7cbeb6cc53b71502d275fa20a42adec0a06d0
halucinka <halucinka@gmail.com>
2018-02-07 18:45:50 -0500

    Clean up logic for stack dfs.

--- DFS/DFS_using_stack/depth_first_search.go
+++ DFS/DFS_using_stack/depth_first_search.go
@@ -1,5 +1,10 @@
 package DFS_using_stack
 
+type Vertex struct {
+	id                       int
+	firstNotVisitedNeighbour int
+}
+
 func GetNumberOfComponents(graph [][]int) int {
 	componentsCount := DFS(graph)
 	return componentsCount
@@ -7,37 +12,32 @@ func GetNumberOfComponents(graph [][]int) int {
 
 func DFS(graph [][]int) int {
 	N := len(graph)
-	visited := make([]bool, N)
+	colors := make([]int, N)
 	for i := 0; i < N; i++ {
-		visited[i] = false
+		colors[i] = 0 // white
 	}
-	stepNumber := 0
 	componentsCount := 0
 
 	for i := 0; i < N; i++ {
-		if !visited[i] {
+		if colors[i] == 0 {
 			componentsCount++
-			stack := []int{i}
-			visited[i] = true
+			stack := []*Vertex{{id: i, firstNotVisitedNeighbour: 0}}
+			colors[i] = 1 //grey
 
 			for len(stack) > 0 {
 				//loop up
 				u := stack[len(stack)-1]
 
-				stepNumber++
-				someNeighbourIsNotVisited := false
-				for _, neighbour := range graph[u] {
-					if !visited[neighbour] {
-						// we pick one neighbour at a time, so every node will be in stack at most once
-						visited[neighbour] = true
-						stack = append(stack, neighbour)
-						someNeighbourIsNotVisited = true
-						break
-					}
-				}
-				if !someNeighbourIsNotVisited {
-					// leaving node = node becomes black
+				if u.firstNotVisitedNeighbour >= len(graph[u.id]) {
 					stack = stack[0 : len(stack)-1]
+					colors[u.id] = 2 // black
+				} else {
+					neighbour := graph[u.id][u.firstNotVisitedNeighbour]
+					u.firstNotVisitedNeighbour++
+					if colors[neighbour] == 0 {
+						colors[neighbour] = 1
+						stack = append(stack, &Vertex{id: neighbour, firstNotVisitedNeighbour: 0})
+					}
 				}
 			}
 		}
