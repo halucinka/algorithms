e4867b1326c123307a158dd1e52c4aa031ca8c47
halucinka <halucinka@gmail.com>
2018-02-07 14:36:30 -0500

    Split 2 implementations of dfs to separate packages.

--- DFS/DFS_using_recursion/depth_first_search.go
+++ DFS/DFS_using_recursion/depth_first_search.go
@@ -1,45 +1,4 @@
-package DFS
-
-func GetNumberOfComponents(graph [][]int) int {
-	componentsCount := DFS(graph)
-	return componentsCount
-}
-
-func DFS(graph [][]int) int {
-	N := len(graph)
-	visited := make([]bool, N)
-	for i := 0; i < N; i++ {
-		visited[i] = false
-	}
-	stepNumber := 0
-	componentsCount := 0
-
-	for i := 0; i < N; i++ {
-		if !visited[i] {
-			componentsCount++
-			stack := []int{i}
-			visited[i] = true
-
-			for len(stack) > 0 {
-				//pop
-				u := stack[len(stack)-1]
-				stack = stack[0 : len(stack)-1]
-
-				stepNumber++
-				for _, neighbour := range graph[u] {
-					if !visited[neighbour] {
-						// we pick one neighbour at a time, so every node will be in stack at most once
-						stack = append(stack, i)
-						visited[neighbour] = true
-						stack = append(stack, neighbour)
-						break
-					}
-				}
-			}
-		}
-	}
-	return componentsCount
-}
+package DFS_using_recursion
 
 var stepNumber int
 var discoveryOrderingIn = []int{}
--- DFS/DFS_using_recursion/depth_first_search_test.go
+++ DFS/DFS_using_recursion/depth_first_search_test.go
@@ -1,50 +1,10 @@
-package DFS
+package DFS_using_recursion
 
 import (
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
-
-func Test_GetNumberOfComponents(t *testing.T) {
-
-	Convey("Test GetNumberOfComponents - one component", t, func() {
-		graph := [][]int{
-			{1, 2},
-			{0, 2},
-			{0, 3},
-			{2},
-		}
-		So(GetNumberOfComponents(graph), ShouldEqual, 1)
-	})
-	Convey("Test GetNumberOfComponents - 2 components, isolated vertex", t, func() {
-		graph := [][]int{
-			{1, 2},
-			{0, 2},
-			{0},
-			{},
-		}
-		So(GetNumberOfComponents(graph), ShouldEqual, 2)
-	})
-	Convey("Test GetNumberOfComponents - 2 components", t, func() {
-		graph := [][]int{
-			{1},
-			{0},
-			{3},
-			{2},
-		}
-		So(GetNumberOfComponents(graph), ShouldEqual, 2)
-	})
-	Convey("Test GetNumberOfComponents - 4 components, no edges", t, func() {
-		graph := [][]int{
-			{},
-			{},
-			{},
-			{},
-		}
-		So(GetNumberOfComponents(graph), ShouldEqual, 4)
-	})
-}
 
 func Test_GetNumberOfComponents_recursive(t *testing.T) {
 
